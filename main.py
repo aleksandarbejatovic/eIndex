62580dad68dfd8fdb125302092b9c391603962a3
aleksandarbejatovic <aleksandar.bejatovic@gmail.com>
2020-10-23 14:33:25 +0200

    app.delete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from fastapi import FastAPI, Query
 from typing import List, Optional
 
+import sys
 import datetime
 import pickle
 
@@ -10,12 +11,14 @@ app = FastAPI()
 
 
 class IspitIn(BaseModel):
+    id: int #redni broj ispita u datoj skolskoj godini 1,2,3,4,5....
     vrijeme: datetime.date
     mjesto: str
     isPolozen: bool = False
     isPrijavljen: bool = False
     isPrisustvovao: bool = False
     bodovi: int = 50
+    vidljiv: bool = True #promjenjivu vidljiv koristim za soft delete, u slucaju brisanja, setuje se samo vidljiv na false
 
 
 class PredmetIn(BaseModel):
@@ -28,7 +31,7 @@ class PredmetIn(BaseModel):
     vjezbe_vrijeme: datetime.date
     lab_mjesto: str
     lab_vrijeme: datetime.datetime
-    ispiti: Optional[IspitIn] = None
+    ispiti: List[Optional[IspitIn]] = None
 
 
 class IspitOut(BaseModel):
@@ -41,8 +44,9 @@ class PredmetOut(BaseModel):
     ispiti: IspitOut
 
 
-hes_predmeta = dict()
-hes_ispita = dict()
+hes_predmeta = dict() #svi predmeti globalna promjenjiva, za naziv predmeta dobijam predmet
+hes_ispita = dict() #svi ispiti, za id ispita dobijam ispit
+hes_ispit_predmet = dict() #ispiti - predmeti, za dati ispit dobijamo predmet
 
 
 @app.post("/dodaj-predmet/", response_model=PredmetOut)
@@ -53,17 +57,61 @@ async def create_predmet(predmet: PredmetIn):
 
 
 @app.post("/dodaj-ispit/", response_model=IspitOut)
-async def create_ispit(ispit: IspitIn):
-    hes_ispita[ispit.naziv] = ispit
+async def create_ispit(ispit: IspitIn, naziv_predmeta: str):
+    if hes_predmeta[str]:
+        hes_predmeta[naziv_predmeta].ispiti.add(ispit)
+        hes_ispita[ispit.id] = ispit
+        hes_ispit_predmet[ispit] = hes_predmeta[naziv_predmeta]
+    else:
+        print("Ne postoji dati predmet!")
     return ispit
 
 
 @app.delete("/obrisi-predmet/")
 async def delete_predmet(predmet: str):
-    hes_predmeta[predmet.naziv] = None  #da li je dovoljno samo odraditi pop ili je potrebnno i del lista_predmeta(i)
+    if hes_predmeta[predmet]:
+        for ispit in hes_predmeta[predmet].ispiti: #brisemo sve ispite za dati predmet
+            hes_ispita[ispit.id] = None
+            hes_ispit_predmet[ispit.id] = None
+        hes_predmeta[predmet] = None
+    else:
+        print("Ne postoji dati predmet!")
 
 
-@app.delete("/obrisi-ispit/")
-async def delete_ispit(ispit: str):
-    predmet = hes_ispita[ispit]
-    hes_predmeta[predmet] = None
+@app.delete("/obrisi-ispit-hard/") #hard delete ispita
+async def delete_ispit(ispit: int):
+    if hes_ispita[ispit]:
+        predmet = hes_ispit_predmet[ispit] #na kojem predmetu se nalazi ispit, da ga obrisemo
+        hes_predmeta[predmet].ispiti.remove(hes_ispita[ispit]) #brisanje ispita sa datog predmeta
+        hes_ispita[ispit] = None #brisanje ispita iz hesa svih ispita
+        hes_ispit_predmet[ispit] = None #jer dati ispit vise ne postoji
+    else:
+        print("Ne postoji dati ispit!")
+
+
+@app.delete("/obrisi-ispit-soft/") #soft delete ispita
+async def delete_ispit(ispit: int):
+    if hes_ispita[ispit] and hes_ispita[ispit].vidljiv == True:
+        hes_ispita[ispit].vidljiv = False
+        i = 0
+        while i < len(hes_ispit_predmet[ispit].ispiti):
+            if(hes_ispit_predmet[ispit].ispiti[i].id == ispit):
+                hes_ispit_predmet[ispit].ispiti[i].vidljiv = False
+                i = len(hes_ispit_predmet[ispit].ispiti)
+            i+=1
+    else:
+        print("Ispit ne postoji ili je obrisan!")
+
+
+@app.put("/edit-predmet/")
+async def edit_predmet(predmet: str, edit: str): #edit -> sta editujemo u predmetu
+    hes_predmeta[predmet].edit = sys.argv[1]
+
+
+@app.on_event("startup")
+async def startup_event():
+    file = open(r"C:\\datoteka.txt", "r") #potrebno dovrsiti
+
+@app.on_event("shutdown")
+def shutdown_event():
+    file = open(r"C:\\datoteka.txt", "w") #potrebno dovrsiti
