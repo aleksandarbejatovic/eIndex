e0b3f24fab10d79aa9a315266d884e98e862f7b0
aleksandarbejatovic <aleksandar.bejatovic@gmail.com>
2020-10-24 23:34:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,25 +43,26 @@ class PredmetOut(BaseModel):
     profesor: str
     ispiti: IspitOut
 
+class hesObject():
+    hes_predmeta = dict() # svi predmeti globalna promjenjiva, za naziv predmeta dobijam predmet
+    hes_ispita = dict() # svi ispiti, za id ispita dobijam ispit
+    hes_ispit_predmet = dict() #ispiti - predmeti, za dati ispit dobijamo predmet
 
-hes_predmeta = dict() #svi predmeti globalna promjenjiva, za naziv predmeta dobijam predmet
-hes_ispita = dict() #svi ispiti, za id ispita dobijam ispit
-hes_ispit_predmet = dict() #ispiti - predmeti, za dati ispit dobijamo predmet
-
+hes = hesObject()
 
 @app.post("/dodaj-predmet/", response_model=PredmetOut)
 async def create_predmet(predmet: PredmetIn):
    #predmet.naziv = Query(min_length=3, default="Matematika") #da li funkcionise da je minimalna duzina svakog predmeta 3
-    hes_predmeta[predmet.naziv] = predmet
+    hes.hes_predmeta[predmet.naziv] = predmet
     return predmet
 
 
 @app.post("/dodaj-ispit/", response_model=IspitOut)
 async def create_ispit(ispit: IspitIn, naziv_predmeta: str):
-    if hes_predmeta[str]:
-        hes_predmeta[naziv_predmeta].ispiti.add(ispit)
-        hes_ispita[ispit.id] = ispit
-        hes_ispit_predmet[ispit] = hes_predmeta[naziv_predmeta]
+    if hes.hes_predmeta[str]:
+        hes.hes_predmeta[naziv_predmeta].ispiti.add(ispit)
+        hes.hes_ispita[ispit.id] = ispit
+        hes.hes_ispit_predmet[ispit] = hes.hes_predmeta[naziv_predmeta]
     else:
         print("Ne postoji dati predmet!")
     return ispit
@@ -69,35 +70,35 @@ async def create_ispit(ispit: IspitIn, naziv_predmeta: str):
 
 @app.delete("/obrisi-predmet/")
 async def delete_predmet(predmet: str):
-    if hes_predmeta[predmet]:
-        for ispit in hes_predmeta[predmet].ispiti: #brisemo sve ispite za dati predmet
-            hes_ispita[ispit.id] = None
-            hes_ispit_predmet[ispit.id] = None
-        hes_predmeta[predmet] = None
+    if hes.hes_predmeta[predmet]:
+        for ispit in hes.hes_predmeta[predmet].ispiti: #brisemo sve ispite za dati predmet
+            hes.hes_ispita[ispit.id] = None
+            hes.hes_ispit_predmet[ispit.id] = None
+        hes.hes_predmeta[predmet] = None
     else:
         print("Ne postoji dati predmet!")
 
 
 @app.delete("/obrisi-ispit-hard/") #hard delete ispita
 async def delete_ispit(ispit: int):
-    if hes_ispita[ispit]:
-        predmet = hes_ispit_predmet[ispit] #na kojem predmetu se nalazi ispit, da ga obrisemo
-        hes_predmeta[predmet].ispiti.remove(hes_ispita[ispit]) #brisanje ispita sa datog predmeta
-        hes_ispita[ispit] = None #brisanje ispita iz hesa svih ispita
-        hes_ispit_predmet[ispit] = None #jer dati ispit vise ne postoji
+    if hes.hes_ispita[ispit]:
+        predmet = hes.hes_ispit_predmet[ispit] #na kojem predmetu se nalazi ispit, da ga obrisemo
+        hes.hes_predmeta[predmet].ispiti.remove(hes.hes_ispita[ispit]) #brisanje ispita sa datog predmeta
+        hes.hes_ispita[ispit] = None #brisanje ispita iz hesa svih ispita
+        hes.hes_ispit_predmet[ispit] = None #jer dati ispit vise ne postoji
     else:
         print("Ne postoji dati ispit!")
 
 
 @app.delete("/obrisi-ispit-soft/") #soft delete ispita
 async def delete_ispit(ispit: int):
-    if hes_ispita[ispit] and hes_ispita[ispit].vidljiv == True:
-        hes_ispita[ispit].vidljiv = False
+    if hes.hes_ispita[ispit] and hes.hes_ispita[ispit].vidljiv == True:
+        hes.hes_ispita[ispit].vidljiv = False
         i = 0
-        while i < len(hes_ispit_predmet[ispit].ispiti):
-            if(hes_ispit_predmet[ispit].ispiti[i].id == ispit):
-                hes_ispit_predmet[ispit].ispiti[i].vidljiv = False
-                i = len(hes_ispit_predmet[ispit].ispiti)
+        while i < len(hes.hes_ispit_predmet[ispit].ispiti):
+            if(hes.hes_ispit_predmet[ispit].ispiti[i].id == ispit):
+                hes.hes_ispit_predmet[ispit].ispiti[i].vidljiv = False
+                i = len(hes.hes_ispit_predmet[ispit].ispiti)
             i+=1
     else:
         print("Ispit ne postoji ili je obrisan!")
@@ -105,13 +106,18 @@ async def delete_ispit(ispit: int):
 
 @app.put("/edit-predmet/")
 async def edit_predmet(predmet: str, edit: str): #edit -> sta editujemo u predmetu
-    hes_predmeta[predmet].edit = sys.argv[1]
+    hes.hes_predmeta[predmet].edit = sys.argv[1]
 
 
-@app.on_event("startup")
+@app.on_event("startup") #deserijalizacija
 async def startup_event():
-    file = open(r"C:\\datoteka.txt", "r") #potrebno dovrsiti
+    infile = open(r"C:\\datoteka.dat", "rb")
+    new_ob = pickle.load(infile)
+    infile.close()
 
-@app.on_event("shutdown")
+
+@app.on_event("shutdown") #serijalizacija
 def shutdown_event():
-    file = open(r"C:\\datoteka.txt", "w") #potrebno dovrsiti
+    outfile = open(r"C:\\datoteka.dat", "wb")
+    pickle.dump(hes, outfile)
+    outfile.close()
